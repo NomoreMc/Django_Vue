9a3fcd44213d970e7bacf8d9eea34cb92fdffef4
nomore <nomoremc@163.com>
2024-08-26 17:36:31 +0800

    add code

--- Post/serializer.py
+++ Post/serializer.py
@@ -4,14 +4,29 @@ from .models import Post
 from Account.models import DefaultUser
 
 
-class PostSerializer(serializers.HyperlinkedModelSerializer):
-    author = serializers.StringRelatedField()
+# class PostSerializer(serializers.HyperlinkedModelSerializer):
+#     author = serializers.StringRelatedField()
+#     # author = serializers.PrimaryKeyRelatedField(queryset=DefaultUser.objects.all())
+#     # author = serializers.SlugRelatedField(queryset=DefaultUser.objects.all(), slug_field='username')
+#     # author = serializers.HyperlinkedRelatedField(
+#     #     view_name='user-detail',
+#     #     read_only=True,
+#     #     lookup_field='username')
+#     class Meta:
+#         model = Post
+#         # fields = ['title', 'content', 'author', 'date_posted']
+#         fields = '__all__'
+        
+        
+class PostSerializer(serializers.ModelSerializer):
+    # author = serializers.StringRelatedField()
     # author = serializers.PrimaryKeyRelatedField(queryset=DefaultUser.objects.all())
     # author = serializers.SlugRelatedField(queryset=DefaultUser.objects.all(), slug_field='username')
     # author = serializers.HyperlinkedRelatedField(
     #     view_name='user-detail',
     #     read_only=True,
     #     lookup_field='username')
+    url = serializers.HyperlinkedIdentityField(view_name='post-detail')
     class Meta:
         model = Post
         # fields = ['title', 'content', 'author', 'date_posted']
